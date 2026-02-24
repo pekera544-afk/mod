b8bbbbf7983220fc761ac87b7a25966304068862
fix <fix@fix.com>
2026-02-25 02:02:17 +0300

    fix: remove prisma.config.js to fix Prisma 7 build error

--- prisma.config.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-/** @type {import('prisma/config').PrismaConfig} */
-module.exports = {
-  schema: 'prisma/schema.prisma',
-  datasourceUrl: process.env.DATABASE_URL,
-};
