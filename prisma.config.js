88783a350eb2585daf213073264751d6ea15a098
fix <fix@fix.com>
2026-02-25 01:54:08 +0300

    fix: prisma generate in startCommand + fix prisma.config.js datasourceUrl format

--- prisma.config.js
+++ prisma.config.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 
+/** @type {import('prisma/config').PrismaConfig} */
 module.exports = {
   schema: 'prisma/schema.prisma',
-  migrations: {
-    path: 'prisma/migrations',
-  },
-  datasource: {
-    url: process.env.DATABASE_URL,
-  },
+  datasourceUrl: process.env.DATABASE_URL,
 };
